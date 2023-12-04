f227858ca62c0a1a1dcdcfe4f4c7c00df45e1ddc
Iain Sandoe <iain@sandoe.co.uk>
2023-12-04 23:58:57 +0000

    gcc: Re-add option handling used for Java.

--- gcc/tree.c
+++ gcc/tree.c
@@ -5108,16 +5108,16 @@ free_lang_data_in_type (tree type)
 	  /* C++ FE uses TREE_PURPOSE to store initial values.  */
 	  TREE_PURPOSE (p) = NULL;
 	}
-      /* Java uses TYPE_MINVAL for TYPE_ARGUMENT_SIGNATURE.  */
-      TYPE_MINVAL (type) = NULL;
+      /* Java uses TYPE_TYPE_MIN_VALUE_RAW for TYPE_ARGUMENT_SIGNATURE.  */
+      TYPE_MIN_VALUE_RAW (type) = NULL;
     }
   else if (TREE_CODE (type) == METHOD_TYPE)
     {
       for (tree p = TYPE_ARG_TYPES (type); p; p = TREE_CHAIN (p))
         /* C++ FE uses TREE_PURPOSE to store initial values.  */
         TREE_PURPOSE (p) = NULL;
-      /* Java uses TYPE_MINVAL for TYPE_ARGUMENT_SIGNATURE.  */
-      TYPE_MINVAL (type) = NULL;
+      /* Java uses TYPE_MIN_VALUE_RAW for TYPE_ARGUMENT_SIGNATURE.  */
+      TYPE_MIN_VALUE_RAW (type) = NULL;
     }
   else if (RECORD_OR_UNION_TYPE_P (type))
     {
@@ -13666,13 +13666,13 @@ verify_type (const_tree t)
 				     TREE_TYPE (TYPE_MIN_VALUE (t))
 	 but does not for C sizetypes in LTO.  */
     }
-  /* Java uses TYPE_MINVAL for TYPE_ARGUMENT_SIGNATURE.  */
-  else if (TYPE_MINVAL (t)
+  /* Java uses TYPE_MIN_VALUE_RAW for TYPE_ARGUMENT_SIGNATURE.  */
+  else if (TYPE_MIN_VALUE_RAW (t)
 	   && ((TREE_CODE (t) != METHOD_TYPE && TREE_CODE (t) != FUNCTION_TYPE)
 	       || in_lto_p))
     {
-      error ("TYPE_MINVAL non-NULL");
-      debug_tree (TYPE_MINVAL (t));
+      error ("TYPE_MIN_VALUE_RAW non-NULL");
+      debug_tree (TYPE_MIN_VALUE_RAW (t));
       error_found = true;
     }
 
